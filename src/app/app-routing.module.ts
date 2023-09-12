ce211641ea0932c67c86eec773b38e162543aa57
Reckhoner <reckhoner@gmail.com>
2023-09-12 12:02:13 -0600

    Componentes y ruteo basico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Menu1Component } from './menu1/menu1.component';
+import { Menu2Component } from './menu2/menu2.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'inicio'},
+  {path: 'menu1', component: Menu1Component },
+  {path: 'menu2', component: Menu2Component },
+  {path: 'inicio', component: InicioComponent},
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Menu1Component } from './menu1/menu1.component';
+import { Menu2Component } from './menu2/menu2.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Menu1Component,
+    Menu2Component,
+    InicioComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
